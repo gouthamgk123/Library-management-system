8b1e93e57fa15cc6823e4c7de6f66c5f1dcd2cad
goutham gk <goutham777gk@gmail.com>
2023-04-03 10:57:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+import './App.css';
+import LandingPage from './components/LandingPage';
+import AdminLogin from './components/AdminLogin';
+import UserLogin from './components/UserLogin';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/'element ={<LandingPage/>}/>
+        <Route path='admin login'element ={<AdminLogin/>}/>
+        <Route path='user login'element ={<UserLogin/>}/>
+
+
+
+        </Routes>
+        </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
